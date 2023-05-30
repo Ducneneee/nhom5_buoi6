e3a46c459dd06b6a4fdf19a5ed3a8ef3e4da3725
Ducneneee <133735038+Ducneneee@users.noreply.github.com>
2023-05-30 11:34:48 +0700

    nopbaitap

---
+++ src/main/java/com/hoangyen/controller/ProductController.java
@@ -0,0 +1,79 @@
+package com.hoangyen.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.hoangyen.model.Product;
+import com.hoangyen.service.ProductService;
+
+
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping("")
+	public String index(Model model) {
+		model.addAttribute("listProduct", productService.GetAll());
+		return "products/index";
+	}
+	
+	@GetMapping("/create")
+    public String createModelForm (Model model) {
+    	model.addAttribute("product", new Product());
+    	return "products/add";
+    }
+    
+    @PostMapping("/create")
+    public String create( @ModelAttribute("product") Product product, @RequestParam("file") MultipartFile file ) {
+    	String fileName = file.getOriginalFilename();
+    	try {
+			file.transferTo( new File("C:\\upload\\" + fileName));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+    	product.setImage(fileName);
+        productService.add(product);
+        return "redirect:/products";
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String editBookForm(@PathVariable("id") int id, Model model){
+        Product editProduct = productService.getProductByID(id);
+        if(editProduct != null){
+            model.addAttribute("product", editProduct);
+            return "products/edit";
+        }else {
+            return "not-found";
+        }
+    }
+    @PostMapping("/edit")
+    public String editBook(@ModelAttribute("product")Product updateProcudt ){
+    	productService.GetAll().stream()
+                .filter(product -> product.getId() == updateProcudt.getId())
+                .findFirst()
+                .ifPresent(product -> {
+                	productService.update(updateProcudt);
+                });
+        return "redirect:/products";
+    }
+    @PostMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") int id){
+    	productService.delete(id);
+        return "redirect:/products";
+    }
+}
---
+++ src/test/java/com/hoangyen/Buoi6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buoi6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
